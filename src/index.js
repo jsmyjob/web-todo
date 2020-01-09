498dcff0e6359265114a6288b34a9c2e63b4afd9
Admin <admin@MacBook-Pro-Admin.local>
2020-01-09 05:38:06 +0200

    Создание структуры приложения: App -> { AppHeader == SearchBar == TodoList  }

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const App = () => {
+    return (
+        <div>
+            <AppHeader />
+            <SearchBar />
+            <TodoList />
+        </div>
+    )
+}
+
+const AppHeader = () => {
+    return (
+        <h1>Todo App</h1>
+    )
+}
+
+const SearchBar = () => {
+    return (
+        <input placeholder="search"/>
+    )
+}
+
+const TodoList = () => {
+    return (
+        <ul>
+            <li>Todo 1</li>
+            <li>Todo 2</li>
+            <li>Todo 3</li>
+        </ul>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
