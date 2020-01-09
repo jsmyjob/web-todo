8141d5e2057d1c0f59026c15cb0256f7ddf74904
Admin <admin@MacBook-Pro-Admin.local>
2020-01-09 05:27:43 +0200

    Начало

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const TodoList = (  
-    <div>
-      <h1>TodoList</h1>
-      <input placeholder="search" />
-      <ul>
-          <li>Todo 1</li>
-          <li>Todo 2</li>
-          <li>Todo 3</li>
-      </ul>
-    </div>
-  );
-
-ReactDOM.render(TodoList, document.getElementById('root'));
