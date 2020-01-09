265cc68877ba12a8edc29abfe793965a765b7ed6
Admin <admin@MacBook-Pro-Admin.local>
2020-01-09 05:17:44 +0200

    TodoList ничего не возвращает

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('Hello world');
+import React from "react";
+import ReactDOM from "react-dom";
+
+const TodoList = (  
+    <div>
+      <h1>TodoList</h1>
+      <input placeholder="search" />
+      <ul>
+          <li>Todo 1</li>
+          <li>Todo 2</li>
+          <li>Todo 3</li>
+      </ul>
+    </div>
+  );
+
+ReactDOM.render(TodoList, document.getElementById('root'));
