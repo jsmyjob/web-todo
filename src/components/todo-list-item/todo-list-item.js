28348076ec394f00970351e9f0a05e579debf347
IvanSamoiliuk <bessob10@gmail.com>
2020-01-10 16:50:44 +0200

    Добавлено состояние state для todo Item и обработчик для зачеркивания

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -5,15 +5,25 @@ export default class TodoListItem extends Component {
 
   constructor() {
     super();
-    this.onClick = () => {
-      console.log(this.props.label)
+
+    this.state = {
+      done: false
+    }
+    
+    this.onClickLabel = () => {
+      this.setState( {done: true} );
     }
   }
-
 
   render() {
 
     const { label, important = false } = this.props;
+    const { done } = this.state;
+
+    let classNames = 'todo-list-item';
+    if(done) {
+      classNames += ' done';
+    }
 
     const style = {
       color: important ? "steelblue" : "black",
@@ -21,8 +31,10 @@ export default class TodoListItem extends Component {
     };
 
     return (
-      <span className="todo-list-item">
-        <span className="todo-list-item-label" style={style} onClick={this.onClick}>
+      <span className={classNames}>
+        <span className="todo-list-item-label" 
+              style={style} 
+              onClick={this.onClickLabel}>
           {label}
         </span>
 
